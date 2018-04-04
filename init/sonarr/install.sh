ca568a4f4e517d34f399927427b3fb9513e27cec
thorpj <thorpejoe4@gmail.com>
2018-04-04 11:06:36 +0800

    a

---
+++ init/sonarr/install.sh
@@ -0,0 +1,10 @@
+Install all the repo's from mono's official site: 
+http://www.mono-project.com/docs/getting-started/install/linux/
+
+sudo apt-get install libmono-cil-dev
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FDA5DFFC
+sudo echo "deb http://apt.sonarr.tv/ master main" | sudo tee /etc/apt/sources.list.d/sonarr.list
+
+sudo apt-get update
+sudo apt-get -y install  nzbdrone 
