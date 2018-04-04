41639afb5cd48d7d56c704e11d13c73781e966a7
thorpj <thorpejoe4@gmail.com>
2018-04-04 12:01:35 +0800

    a

---
+++ init/deluge/install.sh
@@ -0,0 +1,4 @@
+
+sudo add-apt-repository ppa:deluge-team/ppa
+sudo apt-get update
+sudo apt-get install deluge
--- init/rtorrent/install.sh
+++ init/rtorrent/install.sh
@@ -4,5 +4,5 @@ sudo apt-get install unrar unzip ffmpeg mediainfo curl sqlite3 -y
 cd /var/www/html
 sudo apt-get install git -y && sudo git clone https://github.com/Novik/ruTorrent.git && sudo apt-get purge git -y
 sudo chown -R plex:www-data ruTorrent/ && sudo chmod -R 770 ruTorrent/
-cp ./rtorrent.rc $HOME
+cp ./.rtorrent.rc $HOME
 mkdir $HOME/.rtorrent.session
