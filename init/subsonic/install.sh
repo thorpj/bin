191f75ad9189454cd592052725b19fe8a6caa715
thorpj <thorpejoe4@gmail.com>
2018-04-06 00:23:35 +0800

    a

--- init/subsonic/install.sh
+++ init/subsonic/install.sh
@@ -1,3 +1,5 @@
 wget https://s3-eu-west-1.amazonaws.com/subsonic-public/download/subsonic-6.1.3.deb
-sudo apt -y install openjdk-7-jre
+sudo -y add-apt-repository ppa:openjdk-r/ppa  
+sudo apt-get update   
+sudo apt-get -y install openjdk-7-jdk  
 sudo dpkg -i subsonic*.deb
