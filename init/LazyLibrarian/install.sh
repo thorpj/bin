1e40576deb5f1c757a517c58e39bb76e18412c9a
thorpj <thorpejoe4@gmail.com>
2018-04-06 00:28:23 +0800

    a

--- init/LazyLibrarian/install.sh
+++ init/LazyLibrarian/install.sh
@@ -1,2 +1,2 @@
 wget https://github.com/DobyTang/LazyLibrarian/releases/download/1.5.1/lazylibrarian_1.5.1-1_all.deb
-sudo cp LazyLibrarian.service /etc/systemd/system/lazylibrarian.service
+sudo cp lazylibrarian.service /etc/systemd/system/lazylibrarian.service
