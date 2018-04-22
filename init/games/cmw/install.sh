8f91182f760168b9d81495926b93e2b76859f6be
thorpj <thorpejoe4@gmail.com>
2018-04-22 11:30:25 +0800

    a

--- init/games/cmw/install.sh
+++ init/games/cmw/install.sh
@@ -1,14 +1,14 @@
 sudo dpkg --add-architecture i386
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install build-essential libstdc++6 libstdc++6:i386 libc6 libc6:i386
+sudo apt-get -y upgrade
+sudo apt-get -y install build-essential libstdc++6 libstdc++6:i386 libc6 libc6:i386
 
 mkdir ~/steam
 cd ~/steam
 wget http://media.steampowered.com/installer/steamcmd_linux.tar.gz
 tar zxf steamcmd_linux.tar.gz
 
-~/steam/steamcmd +runscript steamcmd_cmw.txt
+~/steam/steamcmd.sh +runscript steamcmd_cmw.txt
 
 cp steam/linux32/steamclient.so cmw/Binaries/Linux/lib
 echo 219640 > ~/cmw/Binaries/Linux/steam_appid.txt
