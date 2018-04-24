e63c8f0e924eebe99c60cc2f58ff50af21a93bfa
thorpj <thorpejoe4@gmail.com>
2018-04-24 12:55:10 +0800

    a

--- init/bash_init.sh
+++ init/bash_init.sh
@@ -5,5 +5,6 @@ append_path_suffix '$HOME/.rbenv/plugins/ruby-build/bin'
 append_path_suffix '$HOME/.tmuxifier/bin'
 append_path_suffix '$HOME/bin'
 
+add_bashrc_alias 'tail_latest' 'tail -fn 100 "$(ls -at | head -n 1)"'
 add_bashrc_command 'eval "$(rbenv init -)"'
 add_profile_command 'eval "$(tmuxifier init -)"'
