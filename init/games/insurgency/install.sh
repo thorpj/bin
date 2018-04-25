90244a44c800a7507a86726412671d51fbf702b0
thorpj <thorpejoe4@gmail.com>
2018-04-25 16:17:22 +0800

    a

--- init/games/insurgency/install.sh
+++ init/games/insurgency/install.sh
@@ -2,7 +2,7 @@
 # passwd insserver
 # su -insserver
 
-if [ "$user" != "inserver" ]; then
+if [ "$USER" != "insserver" ]; then
     exit
 fi
 
@@ -10,14 +10,6 @@ sudo dpkg --add-architecture i386
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt install -y mailutils postfix curl wget file bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc tmux lib32gcc1 libstdc++6 libstdc++6:i386
-
-
-mkdir ~/steam
-cd ~/steam
-wget http://media.steampowered.com/installer/steamcmd_linux.tar.gz
-tar zxf steamcmd_linux.tar.gz
-
-~/steam/steamcmd.sh +runscript ~/bin/init/games/cmw/steamcmd_ins.txt
 
 wget https://linuxgsm.com/dl/linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh insserver
 ./insserver install
