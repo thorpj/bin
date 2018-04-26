c6b0ae987c50d375e85efba997256d2b0f2ee265
thorpj <thorpejoe4@gmail.com>
2018-04-26 09:06:21 +0800

    a

--- cloneinit.sh
+++ cloneinit.sh
@@ -1,13 +1,24 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt dist-upgrade -y
-git clone git@github.com:thorpj/bin.git $HOME/bin
-bash $HOME/bin/init/init.sh
-
 packages=("git" "curl" "nfs-common")
 for package in "${packages[@]}"; do
     sudo apt -y install $package
 done
+
+git clone git@github.com:thorpj/bin.git $HOME/bin
+bash $HOME/bin/init/init.sh
+
+read -p "new username (can be blank): "
+if [ ! -z "$username" ]; then
+    sudo cp $HOME/.vimrc /home/$username
+    sudo cp -r $HOME/.tmux* /home/$username
+    sudo cp $HOME/.bashrc /home/$username
+    
+    sudo chown "$username:$username" /home/$username/.vimrc
+    sudo chown -R "$username:$username" /home/$username/.tmux*
+    sudo chown "$username:$username" /home/$username/.bashrc
+fi
 
 read -p "hostname: " hostname
 sudo hostname $hostname
