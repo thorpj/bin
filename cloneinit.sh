bba7342ae6f04b71fd30b0ab9c2c3dcc346a287d
thorpj <thorpejoe4@gmail.com>
2018-04-30 08:58:07 +0800

    a

--- cloneinit.sh
+++ cloneinit.sh
@@ -21,11 +21,13 @@ if [ ! -z "$username" ]; then
     sudo cp $HOME/.vimrc /home/$username
     sudo cp -r $HOME/.tmux* /home/$username
     sudo cp $HOME/.bashrc /home/$username
-
+    
     sudo chown "$username:$username" /home/$username/.vimrc
     sudo chown -R "$username:$username" /home/$username/.tmux*
     sudo chown "$username:$username" /home/$username/.bashrc
 fi
+
+sudo usermod -aG sudo "$username"
 
 read -p "hostname: " hostname
 sudo hostname $hostname
@@ -35,7 +37,8 @@ sudo sed -i 's/template.yunolan.me/'$hostname'.yunolan.me/g' /etc/hosts
 sudo sed -i 's/template/'$hostname'/g' /etc/hosts
 
 read -p "ip: " ip
-sudo sed -i 's/[replaceip]/'$ip'/g' /etc/network/interfaces
+sudo cp "$HOME/bin/init/networking" /etc/network/interfaces.d
+sudo sed -i 's/[replaceip]/'$ip'/g' /etc/network/interfaces.d/networking
 
 
 
